577d1b11061645fcf4f9647319dc7f96c0d59418
Hasini Reddy <kandula.hasini009@gmail.com>
2026-01-19 18:02:21 +0530

    Remove typo errors

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ async function main() {
     await mongoose.connect(process.env.MONGO_URL);
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on ${PORT}`));
-    console.log("listening to port 3000");
 }
 
 main();
