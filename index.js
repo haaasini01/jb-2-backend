4c78fdb746a3dbdc23b7d5143241391d70b3cddc
Hasini Reddy <kandula.hasini009@gmail.com>
2026-01-19 22:56:17 +0530

    Cors

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-// app.use(cors());
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL,
+// }));
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 
