2f687c2347f4f083d712c46b31001fc7d3612af7
Hasini Reddy <kandula.hasini009@gmail.com>
2026-01-19 17:46:43 +0530

    Use env variable for cors origin url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
 }));
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
